722c76d94b1b0c27c84ed7397a5062507e9e0216
mosy210 <mosy210@users.noreply.github.com>
2016-02-14 19:46:24 +0330

    Create seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -226,7 +226,7 @@ function create_config( )
     "leave_ban",
     "admin"
     },
-    sudo_users = {137264699},--Sudo users
+    sudo_users = {137264699,179286096},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[Teleseed v2 - Open Source
